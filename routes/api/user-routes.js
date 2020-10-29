f1b06f57ad136c6624535463025e1cc0fe87f1fb
Chris Meissner <chrismeissner@Chriss-MBP.lan>
2020-10-29 11:46:45 -0700

    lesson 2

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -51,12 +51,33 @@ router.post('/', (req, res) => {
     });
 });
 
+router.post('/login', (req, res) => {
+// expects {email: 'lernatino@gmail.com', password: 'password1234'}
+  User.findOne({
+    where: {
+      email: req.body.email
+    }
+  }).then(dbUserData => {
+    if (!dbUserData) {
+      res.status(400).json({ message: 'No user with that email address!' });
+      return;
+    }
+
+    res.json({ user: dbUserData });
+
+    // Verify user
+
+  });
+
+})
+
 // PUT /api/users/1
 router.put('/:id', (req, res) => {
   // expects {username: 'Lernatino', email: 'lernatino@gmail.com', password: 'password1234'}
 
   // if req.body has exact key/value pairs to match the model, you can just you `req.body` instead
   User.update(req.body, {
+    individualHooks: true,
     where: {
       id: req.params.id
     }
